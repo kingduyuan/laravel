b5f8ea614e76803f2529330bcaf06e84d111060f
liujinxing <821901008@qq.com>
2017-11-05 19:57:48 +0800

    liujx 日程管理 - datatables

--- resources/views/admin/calendars/index.blade.php
+++ resources/views/admin/calendars/index.blade.php
@@ -24,41 +24,6 @@
                                 </table>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-sm-5">
-                                <div class="dataTables_info" id="example2_info" role="status" aria-live="polite">Showing 1
-                                    to 10 of 57 entries
-                                </div>
-                            </div>
-                            <div class="col-sm-7">
-                                <div class="dataTables_paginate paging_simple_numbers" id="example2_paginate">
-                                    <ul class="pagination">
-                                        <li class="paginate_button previous disabled" id="example2_previous">
-                                            <a href="#"
-                                               aria-controls="example2"
-                                               data-dt-idx="0"
-                                               tabindex="0">Previous</a>
-                                        </li>
-                                        <li class="paginate_button active"><a href="#" aria-controls="example2"
-                                                                              data-dt-idx="1" tabindex="0">1</a></li>
-                                        <li class="paginate_button "><a href="#" aria-controls="example2" data-dt-idx="2"
-                                                                        tabindex="0">2</a></li>
-                                        <li class="paginate_button "><a href="#" aria-controls="example2" data-dt-idx="3"
-                                                                        tabindex="0">3</a></li>
-                                        <li class="paginate_button "><a href="#" aria-controls="example2" data-dt-idx="4"
-                                                                        tabindex="0">4</a></li>
-                                        <li class="paginate_button "><a href="#" aria-controls="example2" data-dt-idx="5"
-                                                                        tabindex="0">5</a></li>
-                                        <li class="paginate_button "><a href="#" aria-controls="example2" data-dt-idx="6"
-                                                                        tabindex="0">6</a></li>
-                                        <li class="paginate_button next" id="example2_next"><a href="#"
-                                                                                               aria-controls="example2"
-                                                                                               data-dt-idx="7" tabindex="0">Next</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
                 <!-- /.box-body -->
@@ -190,8 +155,8 @@
           href="{{ asset('admin-assets/plugins/iCheck/all.css') }}"/>
 @endpush
 @push("script")
-    <script src="{{ asset('admin-assets/plugins/datatables/DataTables-1.10.12/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('admin-assets/plugins/datatables/DataTables-1.10.12/dataTables.bootstrap.min.js') }}"></script>
+    <script src="{{ asset('admin-assets/plugins/datatables/DataTables-1.10.12/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('admin-assets/plugins/datatables/DataTables-1.10.12/js/dataTables.bootstrap.min.js') }}"></script>
     <script src="{{ asset('admin-assets/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
     <!-- Page specific script -->
     <script>
@@ -200,7 +165,7 @@
                 'paging'      : true,
                 'lengthChange': false,
                 'searching'   : false,
-                'ordering'    : true,
+                'ordering'    : false,
                 'info'        : true,
                 'autoWidth'   : false
             })
